94fc8b332f18ec721b1070b51b3ba4b5d6249538
XDdevv <undefineduser087@gmail.com>
2025-10-21 15:23:02 +0500

    Added ObserveAsEvents helper function to observe one time events via Channel

--- composeApp/src/commonMain/kotlin/zed/rainxch/vocabularyflash/App.kt
+++ composeApp/src/commonMain/kotlin/zed/rainxch/vocabularyflash/App.kt
@@ -1,51 +1,13 @@
 package zed.rainxch.vocabularyflash
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.resources.stringResource
+import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import vocabularyflash.composeapp.generated.resources.Res
-import vocabularyflash.composeapp.generated.resources.app_name
-import vocabularyflash.composeapp.generated.resources.compose_multiplatform
 import zed.rainxch.vocabularyflash.core.presentation.design_system.theme.AppTheme
 
 @Composable
 @Preview
 fun App() {
     AppTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text(stringResource(Res.string.app_name))
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+
     }
 }
