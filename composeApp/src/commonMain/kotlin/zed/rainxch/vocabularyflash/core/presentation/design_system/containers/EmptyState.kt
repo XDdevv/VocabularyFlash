0d6758d674b3fa5106ecb962d6d03fdd584f1342
XDdevv <undefineduser087@gmail.com>
2025-10-22 22:06:30 +0500

    updated EmptyState.kt, added text style

--- composeApp/src/commonMain/kotlin/zed/rainxch/vocabularyflash/core/presentation/design_system/containers/EmptyState.kt
+++ composeApp/src/commonMain/kotlin/zed/rainxch/vocabularyflash/core/presentation/design_system/containers/EmptyState.kt
@@ -13,12 +13,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun EmptyState(
     message: String,
     icon: ImageVector = Icons.Default.SearchOff,
+    textStyle: TextStyle = MaterialTheme.typography.headlineLarge,
     modifier: Modifier = Modifier,
 ) {
     Column(
@@ -35,7 +37,7 @@ fun EmptyState(
 
         Text(
             text = message,
-            style = MaterialTheme.typography.headlineLarge,
+            style = textStyle,
             color = MaterialTheme.colorScheme.onSurface
         )
     }
