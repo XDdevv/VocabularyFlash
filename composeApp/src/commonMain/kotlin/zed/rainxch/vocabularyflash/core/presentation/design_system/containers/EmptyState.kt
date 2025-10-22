bf0e86ddc2bc8e6b723b5bb1a01c120900357ffb
XDdevv <undefineduser087@gmail.com>
2025-10-22 20:53:19 +0500

    Implemented EmptyState.kt

---
+++ composeApp/src/commonMain/kotlin/zed/rainxch/vocabularyflash/core/presentation/design_system/containers/EmptyState.kt
@@ -0,0 +1,42 @@
+package zed.rainxch.vocabularyflash.core.presentation.design_system.containers
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.SearchOff
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun EmptyState(
+    message: String,
+    icon: ImageVector = Icons.Default.SearchOff,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            modifier = Modifier.size(100.dp),
+            tint = MaterialTheme.colorScheme.error
+        )
+
+        Text(
+            text = message,
+            style = MaterialTheme.typography.headlineLarge,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+    }
+}
